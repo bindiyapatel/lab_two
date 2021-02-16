549f40bab00b039fa49c7bca917bfd1f65e7fe90
Bindiya Patel <bindiyapatel7069@gmail.com>
2021-02-16 22:47:41 +0530

    bug fixed

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-    <?php include('header.php'); ?>
+<?php include('header.php'); ?>
     <body>
         <div class="container">
             <div class="wrapper">
@@ -7,13 +7,13 @@
                 </header>
                 <form method="post" action="poem-generator.php">
                     <div class="form-group">
-                        <input type="text" placeholder="Enter your favourite colour" name="colour" class="form-control">
+                        <input type="text" placeholder="Enter your favourite colour" name="colour" class="form-control" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" placeholder="Enter a plural noun" name="noun" class="form-control">
+                        <input type="text" placeholder="Enter a plural noun" name="noun" class="form-control" required>
                     </div>
                     <div class="form-group">
-                        <label> Enter the name of a person </label>
+                        <input type="text" placeholder="Enter a person" name="person" class="form-control" required>
                     </div>
                     <input type="submit" value="submit" class="btn btn-primary" name="submit">
                 </form>
